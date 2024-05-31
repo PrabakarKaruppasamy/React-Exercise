130c8943e7931a108e164e50c8bda0984f5a2299
Krithvik <prabakarkaruppasamy@gmail.com>
2024-05-31 07:13:03 +0530

    Igniting React

---
+++ README.md
@@ -0,0 +1,21 @@
+#React App
+
+
+#Parcel
+ - Dev build
+ - Local Server
+ - HMR = Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Caching - Faster Builds
+ - Image Optimization
+ - Minification
+ - Bundling
+ - Compress
+ - Consistent Hashing
+ - Code Splitting
+ - Differential Building - support old broswrs
+ - Diagnostic
+ - Error Handling
+ - Https
+ - Tree Shaking - remove unused code
+ - Different dev and prod bundle
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 /**
  *  <div id="parent">
  *  <div id="child">
@@ -14,19 +17,19 @@ const parent = React.createElement('div', { id: "parent" },
     [
         React.createElement("div", { id: "child" },
         [
-            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h1", {}, "This is H1 Tag"),
             React.createElement("h2", {}, "I'm an h2 tag")
         ]
     )
     ],
-    [
-        React.createElement("div", { id: "child2" },
-        [
-            React.createElement("h1", {}, "I'm an h1 tag"),
-            React.createElement("h2", {}, "I'm an h2 tag")
-        ]
-    )
-    ]
+    // [
+    //     React.createElement("div", { id: "child2" },
+    //     [
+    //         React.createElement("h1", {}, "I'm an h1 tag"),
+    //         React.createElement("h2", {}, "I'm an h2 tag")
+    //     ]
+    // )
+    // ]
 )
 
 
